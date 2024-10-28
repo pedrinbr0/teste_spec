8398d860e4b5e8981f8a154f01a64cbe8bba31f6
pedro <paulosilvaotaviano@gmail.com>
2024-10-28 18:29:17 -0300

    feat(merge_sort): Com os elementos divididos, agora eles são ordenados do menor elemento para o maior

--- lib/MergeSort.rb
+++ lib/MergeSort.rb
@@ -7,7 +7,38 @@ def merge_sort(arr)
   left = merge_sort(arr[0...mid])  # Da posição 0 até antes do 'mid'
   right = merge_sort(arr[mid..])   # Da posição 'mid' até o final
 
-  return [left, right]
+  merge(left, right)
+end
+
+def merge(left, right)
+  merged = []  # Array para armazenar os elementos mesclados
+  l = 0        # Ponteiro para a lista 'left'
+  r = 0        # Ponteiro para a lista 'right'
+
+  # Enquanto houver elementos em ambas as listas
+  while l < left.length && r < right.length
+    if left[l] <= right[r]
+      merged << left[l]  # Adiciona o menor elemento
+      l += 1             # Move o ponteiro da lista 'left'
+    else
+      merged << right[r] # Adiciona o menor elemento
+      r += 1             # Move o ponteiro da lista 'right'
+    end
+  end
+
+  # Adiciona elementos restantes da lista 'left'
+  while l < left.length
+    merged << left[l]
+    l += 1
+  end
+
+  # Adiciona elementos restantes da lista 'right'
+  while r < right.length
+    merged << right[r]
+    r += 1
+  end
+
+  merged # Retorna a lista mesclada
 end
 
 arr = [38, 27, 43, 10]
