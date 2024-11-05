576d65eddb9002375933ffa0085e60e995a59ed6
pedro <paulosilvaotaviano@gmail.com>
2024-11-05 16:52:14 -0300

    Retirando # desnecessarias

--- lib/MergeSort.rb
+++ lib/MergeSort.rb
@@ -15,30 +15,30 @@ def merge(left, right)
   l = 0        # Ponteiro para a lista 'left'
   r = 0        # Ponteiro para a lista 'right'
 
-  # Enquanto houver elementos em ambas as listas
+
   while l < left.length && r < right.length
     if left[l] <= right[r]
-      merged << left[l]  # Adiciona o menor elemento
-      l += 1             # Move o ponteiro da lista 'left'
+      merged << left[l]
+      l += 1
     else
-      merged << right[r] # Adiciona o menor elemento
-      r += 1             # Move o ponteiro da lista 'right'
+      merged << right[r]
+      r += 1
     end
   end
 
-  # Adiciona elementos restantes da lista 'left'
+
   while l < left.length
     merged << left[l]
     l += 1
   end
 
-  # Adiciona elementos restantes da lista 'right'
+
   while r < right.length
     merged << right[r]
     r += 1
   end
 
-  merged # Retorna a lista mesclada
+  merged
 end
 
 arr = [38, 27, 43, 10]
