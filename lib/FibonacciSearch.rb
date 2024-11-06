d6a7d4f098e31b18d257460112fca7e7bb2e5671
pedro <paulosilvaotaviano@gmail.com>
2024-11-06 15:49:59 -0300

    Feat(fibonacci_search): O código está pronto para uso e puts mais esclarecedor.

--- lib/FibonacciSearch.rb
+++ lib/FibonacciSearch.rb
@@ -42,5 +42,10 @@ arr = [10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100, 235]
 n = arr.length
 x = 235
 
-puts FibonacciSearch( arr, x, n )
+ind = FibonacciSearch( arr, x, n )
 
+if ind >= 0
+  puts "Encontrado no index #{ind}!"
+else
+  puts "#{x} não foi encontrado no array."
+end
