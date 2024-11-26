9e68a8f84b705c91264bcf680f45d60059be4a47
pedro <paulosilvaotaviano@gmail.com>
2024-11-26 16:05:30 -0300

    Refactor(tomohiko_sakamoto): Agora é mais esclarecedor ao rodar o codigo.

--- lib/TomohikoSakamoto.rb
+++ lib/TomohikoSakamoto.rb
@@ -3,7 +3,11 @@ def tomohiko_sakamoto(y, m, d)
 
   t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ]
 
-  (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7
+  day_of_week = (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7
+
+  days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"]
+
+  "A data #{d}/#{m}/#{y + (m < 3 ? 1 : 0)} foi/será um(a) #{days[day_of_week]}."
 end
 
 day = 13
